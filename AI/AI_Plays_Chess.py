571574338352f9abee87cd051ec3be90cb0c0ce2
Manan Sharma <3783977+mananshr@users.noreply.github.com>
2024-10-24 18:14:43 +0530

    Content fixed 2

--- AI/AI_Plays_Chess.py
+++ AI/AI_Plays_Chess.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.title("AI plays chess")
+st.title("AI plays chess, AI ELO over the years")
 
 df = pd.read_csv("data/ai/computer-chess-ability.csv")
 
-fig = px.line(df, x="Year", y="Elo rating", title="Elo rating of AI over the years")
+fig = px.area(df, x="Year", y="Elo rating", title="Elo rating of AI over the years")
 st.plotly_chart(fig)
