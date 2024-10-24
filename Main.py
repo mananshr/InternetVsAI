feac5993378d615364e486ba27f0741e1bddf3bb
Manan Sharma <3783977+mananshr@users.noreply.github.com>
2024-10-24 17:59:43 +0530

    AI Plays chess

--- Main.py
+++ Main.py
@@ -6,11 +6,12 @@ internet_mobile_phone = st.Page("Internet/Mobile_Phone.py")
 
 ai_growth = st.Page("AI/Growth.py")
 ai_jobs = st.Page("AI/Jobs.py")
+ai_chess = st.Page("AI/AI_Plays_Chess.py")
 
 pg = st.navigation(
     {
         "Internet": [internet_coverage, internet_broadband, internet_mobile_phone],
-        "AI": [ai_growth, ai_jobs],
+        "AI": [ai_growth, ai_jobs, ai_chess],
         "comparison": []
     }
 )
