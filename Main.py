99b3617443f4634fe1763b0c10d9e256e16761a0
Manan Sharma <3783977+mananshr@users.noreply.github.com>
2024-10-24 17:52:15 +0530

    Mobile phone usage

--- Main.py
+++ Main.py
@@ -2,15 +2,16 @@ import streamlit as st
 
 internet_coverage = st.Page("Internet/Coverage.py")
 internet_broadband = st.Page("Internet/Broadband.py")
+internet_mobile_phone = st.Page("Internet/Mobile Phone.py")
 
 ai_growth = st.Page("AI/Growth.py")
 ai_jobs = st.Page("AI/Jobs.py")
 
 pg = st.navigation(
     {
-        "Internet": [internet_coverage, internet_broadband],
+        "Internet": [internet_coverage, internet_broadband, internet_mobile_phone],
         "AI": [ai_growth, ai_jobs],
-        "Comparision": []
+        "comparison": []
     }
 )
 pg.run()
